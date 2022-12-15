991049d5fdfd502253f6aa3c2cf74caa3a6d25d6
Manolis Chatzakis <chatzakis@ics.forth.gr>
2022-12-15 17:56:54 +0100

    Update graph-vis.js

--- graph/graph-vis.js
+++ graph/graph-vis.js
@@ -1,6 +1,7 @@
 URL = "https://github.com/yannisban/wikispeedia-link-position/blob/main/graph/graph.json"
 
 const myGraph = ForceGraph3D();
-myGraph(/*<myDOMElement*//*document.getElementById("graph-vis-id")*/"graph-vis-id")
+myGraph(/*<myDOMElement*/document.getElementById("graph-vis-id"))
   //.graphData(URL);
-  .jsonUrl(URL)
+  . 
+  jsonUrl(URL)
